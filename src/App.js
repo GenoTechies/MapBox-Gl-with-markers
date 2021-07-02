bd5d69f98c5d91262629e1746369dd13733a63c6
Yasas770 <64302398+Yasas770@users.noreply.github.com>
2021-07-02 20:37:40 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Map from "./map";
+import logo from "./Assets/img/logo.jpeg";
 
 function App() {
   return (
     <div className="App">
-      <h1>Map (GenoTech)</h1>
-
-      <div className="map">
-        <Map />
+      <h1>React with Mapbox GL</h1>
+      <div className="container">
+        <div className="map">
+          <Map />
+        </div>
+        <div className="logo">
+          <img src={logo} width="400px" height="400px"></img>
+        </div>
       </div>
     </div>
   );
